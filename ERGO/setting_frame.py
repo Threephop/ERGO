f4385c196822ccfe3b126a0fefbcf24b171c6c74
Threephop <150276224+Threephop@users.noreply.github.com>
2025-01-07 16:37:20 +0700

    test

--- ERGO/setting_frame.py
+++ ERGO/setting_frame.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-class SettingFrame(tk.Frame):
+class setting_frame(tk.Frame):
     def init(self, parent):
         super().init(parent, bg="white")
         tk.Label(self, text="You clicked Setting", font=("Arial", 24), bg="white").pack(expand=True)
